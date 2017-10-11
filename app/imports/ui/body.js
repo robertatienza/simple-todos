f568606e26c6c27ed402549d7e17ab675233b722
robertatienza <31534223+robertatienza@users.noreply.github.com>
2017-10-10 23:07:45 -1000

    Task 5: Security with methods, Filtering data with publish and subscribe

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -7,6 +7,7 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
@@ -33,14 +34,9 @@ Template.body.events({
     const target = event.target;
     const text = target.text.value;
 
-
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
+    Meteor.call('tasks.insert', text);
+
 
     // Clear form
     target.text.value = '';
